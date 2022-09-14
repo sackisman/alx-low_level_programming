884870bf9ec6ad62677ae0f87d4b3999e5f8e17b
sackisman <ostechcomm@gmail.com>
2022-09-13 20:55:18 -0700

    tasks0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the world "-putchar"
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = _putchar";
+
+	for (i=0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('/n');
+
+	return (0);
+}
