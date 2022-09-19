4907e5c351e084e977fe369975c9f406f797ef30
sackisman <ostechcomm@gmail.com>
2022-09-19 01:40:32 -0700

    tasks4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,17 @@
 void print_rev(char *s)
 
 {
-	int len = 0, index;
+	int len = 0;
 
-	while (s[index++])
+	while (*(s + len) != '\0')
+	{
 		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	}
+	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
 	_putchar('\n');
 }
