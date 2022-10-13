918b01ab4973daa43903cb15a286a98ce610784e
sackisman <ostechcomm@gmail.com>
2022-10-13 00:03:03 -0700

    tasks3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
 
 
 /**
@@ -22,13 +22,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (*s == *ops[i].op)
-		{
-			return (ops[i].f);
-		}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+	return (ops[i].f);
 }
+
