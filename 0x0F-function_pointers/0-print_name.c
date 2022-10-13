564338cdcbca3da231ee9a6950fa8cff37f80fb3
sackisman <ostechcomm@gmail.com>
2022-10-12 22:25:36 -0700

    tasks0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+
+
+/**
+ * print_name - prints name through passed in function point
+ * @name: character string
+ * @f: function pointer
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (f)
+		f(name);
+}
