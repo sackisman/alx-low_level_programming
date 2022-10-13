d73343279f958b094ffebf370b3232333d5856a6
sackisman <ostechcomm@gmail.com>
2022-10-12 23:45:17 -0700

    tasks1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,13 +10,14 @@
  * @action: function to call for each element of array
  * Return: nothing
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
-	unsigned int i;
-
-	if (array && size > 0 && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
