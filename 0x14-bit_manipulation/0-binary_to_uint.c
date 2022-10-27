94a778363749079f041783b678233832337e3af9
sackisman <ostechcomm@gmail.com>
2022-10-27 14:09:41 -0700

    tasks0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,19 +15,20 @@
 unsigned int binary_to_uint(const char *b)
 
 {
-	unsigned int num = 0, mult = 1;
+	unsigned int total, power;
 	int len;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
-	for (len = 0; b[len];)
-		len++;
-	for (len -= 1; len >= 0; len--)
+	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		num += (b[len] - '0') * mult;
-		mult *= 2;
 	}
-	return (num);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+	return (total);
 }
